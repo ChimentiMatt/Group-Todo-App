0e982e0ba077e578789f26f17c00a56470cad73e
Ashton Smith <ashton23@pdx.edu>
2022-02-21 12:09:30 -0800

    components directory created, header component added imported to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Header from './components/Header.js'
+
+
 
 function App() {
   return (
     <div className="App">
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+        Hello world!
     </h1>
+    <Header></Header>
     </div>
   );
 }
