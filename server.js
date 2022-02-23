b53f252865516436c893cc582269b38194d79388
lisab1993 <brown.lisa999@gmail.com>
2022-02-23 12:14:29 -0800

    404 responses handled

--- server.js
+++ server.js
@@ -10,13 +10,28 @@ mongoose.connect(`mongodb+srv://${secret}@todo-cluster.sar1h.mongodb.net/test`, 
     useUnifiedTopology: true,
 })
 
-.then(() => {
-    console.log('connected to the database')
-    mongoose.connection.on('error', console.error.bind(console, 'error connecting to db'))
+    .then(() => {
+        console.log('connected to the database')
+        mongoose.connection.on('error', console.error.bind(console, 'error connecting to db'))
 
-    app.use(express.json())
+        app.use(express.json())
+    })
+
+//express doesn't consider 404 to be an error (404 means not found isn't technically an error), so this middleware and the one below it will treat 404 responses as errors.
+app.use(function (req, res, next) {
+    let err = new Error('Not Found')
+    err.status = 404
+    next(err)
 })
 
-app.listen(port, function() {
+app.use(function (err, req, res, next) {
+    console.log(err)
+    if (err.status === 404)
+        res.status(404).json({ message: 'not found' })
+    else
+        res.status(500).json({ messsage: 'oops, something went wrong!' })
+})
+
+app.listen(port, function () {
     console.log('server listening on port 8000!!!')
 })
