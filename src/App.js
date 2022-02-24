cbd9e6da501da7bbb3ecf02d79d0c58a7ca1618b
Ashton Smith <ashton23@pdx.edu>
2022-02-23 20:22:41 -0800

    working duedate added for static tasks

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
       id: 1,
       text: 'Walk the dog',
       completed: false,
-      dueDate: 1645673648
+      dueDate: 1644652800000
     },
     {
       id: 2,
       text: 'Exercise',
       completed: false,
-      dueDate: 1645673648
+      dueDate: 1644652800000
     }
   ])
 
